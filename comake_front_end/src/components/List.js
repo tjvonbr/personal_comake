50247d5e61e3c81d4d92a1ce3fa0a74fc3bd6142
Philip Johnson <phil7j@gmail.com>
2019-07-29 12:54:12 -0700

    created basic functional components for project, set up routes

---
+++ comake_front_end/src/components/List.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import axios from 'axios';
+
+function List() {
+    return (
+        <div>
+            <h1>List page</h1>
+        </div>
+    )
+}
+
+export default List;
