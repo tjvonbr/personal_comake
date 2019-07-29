f63a20ae03d07e665795488eb8db3351eff78145
tjvonbr <51134664+tjvonbr@users.noreply.github.com>
2019-07-29 00:20:06 -0500

    Created app and installed a few dependencies

---
+++ comake_front_end/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Co-Make Front End</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
