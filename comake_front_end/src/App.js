6ae4b5eed3eedd529bb1ef1cfdb53386925d9d53
Philip Johnson <phil7j@gmail.com>
2019-07-29 19:38:12 -0700

    add login and register pages, localStorage auth complete

--- comake_front_end/src/App.js
+++ comake_front_end/src/App.js
@@ -1,20 +1,51 @@
-import React from 'react';
+import React, {useState} from 'react';
 import List from './components/List';
 import Register from './components/Register';
 import Login from './components/Login';
 import Profile from './components/Profile';
-import { Route } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
+import {useLocalStorage} from './hooks/useLocalStorage';
 
 function App() {
+  const [token, setToken] = useLocalStorage('token', '')
+  const [message, setMessage] = useState('')
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Co-Make Front End</h1>
       </header>
-    <Route exact path="/" component={List}/>
-    <Route exact path="/profile/:id" component={Profile} />
-    <Route path="/login" component={Login}/>
-    <Route path="/register" component={Register}/>
+
+
+      {/* ROUTES  */}
+
+      <Route exact path="/" render={ props =>
+          localStorage.getItem("token") ? (
+            <List {...props} />
+          ) : (
+            <Redirect to="/login" />
+          )
+        }/>
+      <Route exact path="/profile/:id" render={ props =>
+          localStorage.getItem("token") ? (
+            <Profile {...props} />
+          ) : (
+            <Redirect to="/login" />
+          )
+        } />
+      <Route exact path="/login" render={ props =>
+        <Login setToken={setToken}
+        message={message}
+        setMessage={setMessage}
+        {...props} />}
+        />
+
+      <Route path="/register" render={ props =>
+        <Register setToken={setToken}
+        message={message}
+        setMessage={setMessage}
+        {...props}
+      />}/>
     </div>
   );
 }
--- comake_front_end/src/components/Login.js
+++ comake_front_end/src/components/Login.js
@@ -1,11 +1,78 @@
 import React from 'react'
+import { withFormik, Form, Field, setNestedObjectValues } from "formik";
+import * as Yup from "yup";
+import axios from 'axios';
+// import {Redirect} from 'react-router-dom';
 
-function Login() {
+function Login( {touched, errors, history, message}) {
+
     return (
         <div>
-            <p>Login Page</p>
+
+         <h4>{message}</h4>
+            <p>Login</p>
+
+            <Form className="form">
+      <div className="form-group">
+        <label className="label">Email:</label>
+        <Field
+          className="input"
+          name="email"
+          type="text"
+          autoComplete="off"
+        />
+        <p>{touched.email && errors.email}</p>
+      </div>
+      <div className="form-group">
+        <label className="label">Password:</label>
+        <Field
+          className="input"
+          name="password"
+          type="password"
+          autoComplete="off"
+        />
+      </div>
+      <p>{touched.password && errors.password}</p>
+      <button type="submit" className="btn">Log In </button>
+      <button className="btn" onClick={()=> history.push('/register')}>Register </button>
+    </Form>
         </div>
     )
 }
 
-export default Login;
+export default withFormik({
+
+    mapPropsToValues() {
+      return {
+        email: "",
+        password: ""
+
+      };
+    },
+    validationSchema: Yup.object().shape({
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string()
+        // .min(6)
+        .required()
+    }),
+    handleSubmit(values, formikBag) {
+      console.log(values)
+      const url =
+        "https://co-make.herokuapp.com/auth/login";
+      axios
+        .post(url, values)
+        .then( res => {
+
+          formikBag.props.setMessage('')
+          console.log("Success!", res)
+          formikBag.props.setToken(res.data.token)
+          formikBag.props.history.push("/");
+        })
+
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  })(Login);
