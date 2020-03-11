4bbe5ce32803069442a03d588c147e71783fa17d
tjvonbr <51134664+tjvonbr@users.noreply.github.com>
2020-03-11 14:08:27 -0500

    cleaned up the validate-register middleware

--- middleware/validate-registration.js
+++ middleware/validate-registration.js
@@ -1,12 +1,12 @@
 module.exports = validateRegister;
 
 function validateRegister(req, res, next) {
-  const { email, password, zipCode} = req.body;
+  const { email, username, zipCode, password} = req.body;
 
-  if (!email || !password || !zipCode) {
+  if (!email || !username || !zipCode || !password) {
     res
       .status(400)
-      .json({ message: "You're missing one of the following fields:  Email, username, or password" });
+      .json({ message: "You're missing one of the following fields:  username, email, password, or zip code!" });
   } else {
     next();
   }
