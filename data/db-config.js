9891f56cee45db3e73b84cd9cce8998f3ffdf5bb
tjvonbr <51134664+tjvonbr@users.noreply.github.com>
2020-04-06 20:50:42 -0500

    edited prodcution config variable

--- data/db-config.js
+++ data/db-config.js
@@ -1,5 +1,6 @@
 const knex = require('knex');
 const config = require('../knexfile.js');
 const dbEnv = process.env.DB_ENV || 'development'
+// Add config variable in heroku!
 
 module.exports = knex(config[dbEnv]);
